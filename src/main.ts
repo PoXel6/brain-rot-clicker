27be56bb2564b152c51cc894b50ef8b9f32c6add
pox <iampoxel6@gmail.com>
2025-06-22 11:46:20 +0330

    feat: count increments on button press

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 const btn = document.getElementById('btn')
 const counter = document.getElementById('counter')
 
-btn.addEventListener('click', action)
-function action(this: HTMLButtonElement, _: MouseEvent) {
-  const texts = counter?.textContent?.split(' ')
-  texts[2] = +texts[2] + 1
-  counter.innerText = texts.join(' ')
+btn?.addEventListener('click', incCount)
+function incCount() {
+	setCount(getCount() + 1)
+}
+
+function getCount(): number {
+	const text = counter?.textContent
+	if (!text) {
+		return -1
+	}
+	const texts = text.split(' ')
+	return Number(texts[2])
+}
+
+function setCount(val: number): void {
+	const text = counter?.textContent
+	if (!text) {
+		return
+	}
+	const texts = text.split(' ')
+	texts[2] = val.toString()
+	counter.innerText = texts.join(' ')
 }
