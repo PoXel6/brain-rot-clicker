e1d7b9442815857be4b922b8c896c30c14cda86e
pox <iampoxel6@gmail.com>
2025-06-22 11:54:57 +0330

    feat: add multiplier functionality

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 const btn = document.getElementById('btn')
 const counter = document.getElementById('counter')
+const multiBtn = document.getElementById('inc-multi')
+let def_multi = 1
+let def_multi_cost = 10
 
 btn?.addEventListener('click', incCount)
 function incCount() {
-	setCount(getCount() + 1)
+	setCount(getCount() + def_multi)
 }
 
 function getCount(): number {
@@ -23,4 +26,21 @@ function setCount(val: number): void {
 	const texts = text.split(' ')
 	texts[2] = val.toString()
 	counter.innerText = texts.join(' ')
+}
+
+multiBtn?.addEventListener('click', incMulti)
+function incMulti(): void {
+  if (getCount() < 10) {
+    return
+  }
+  setMulti(getMulti() + 1)
+  setCount(getCount() - def_multi_cost)
+}
+
+function setMulti(val: number): void {
+  def_multi = val
+}
+
+function getMulti(): number {
+  return def_multi
 }
